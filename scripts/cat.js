a6ff60dd8cc74b33d00b38405198a513875ac577
asterisk9101 <tm11b028@gmail.com>
2016-05-01 14:34:44 +0900

    ascii のキャレット記法に対応

--- scripts/cat.js
+++ scripts/cat.js
@@ -61,9 +61,39 @@ function echo(m) {
 function cat(opts, files){
     function control_char(ch){
         switch(ch.charCodeAt(0)){
-        case 9: ch = "^I"; break; // tab
+        case 0: ch = "^@"; break;
+        case 1: ch = "^A"; break;
+        case 2: ch = "^B"; break;
+        case 3: ch = "^C"; break;
+        case 4: ch = "^D"; break;
+        case 5: ch = "^E"; break;
+        case 6: ch = "^F"; break;
+        case 7: ch = "^G"; break;
+        case 8: ch = "^H"; break;
+        case 9: ch = "^I"; break;
         case 10: ch = "$\n"; break; // new line
-        default: ch = ch.charCodeAt(0) < 32 ? ch = "^M" : ch; break;
+        case 11: ch = "^K"; break;
+        case 12: ch = "^L"; break;
+        case 13: ch = "^M"; break;
+        case 14: ch = "^N"; break;
+        case 15: ch = "^O"; break;
+        case 16: ch = "^P"; break;
+        case 17: ch = "^Q"; break;
+        case 18: ch = "^R"; break;
+        case 19: ch = "^S"; break;
+        case 20: ch = "^T"; break;
+        case 21: ch = "^U"; break;
+        case 22: ch = "^V"; break;
+        case 23: ch = "^W"; break;
+        case 24: ch = "^X"; break;
+        case 25: ch = "^Y"; break;
+        case 26: ch = "^Z"; break;
+        case 27: ch = "^["; break;
+        case 28: ch = "^\\"; break;
+        case 29: ch = "^]"; break;
+        case 30: ch = "^^"; break;
+        case 31: ch = "^_"; break;
+        case 127: ch = "^?"; break; // del
         }
         return ch;
     }
